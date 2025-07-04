80de62a184d4373240db7f69765056cd4f0ca0c5
DilettaZecchinetti <diletta.zecchinetti@gmail.com>
2025-07-04 14:45:11 +0100

    Set up Clerk

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,23 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
 import './App.css'
 
 function App() {
 
-
   return (
-    <>
+    <header>
+      <SignedIn>
+        <h1>Welcome, you're signed in</h1>
+        <UserButton />
+      </SignedIn>
 
-    </>
-  )
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+    </header>
+  );
 }
 
 export default App
