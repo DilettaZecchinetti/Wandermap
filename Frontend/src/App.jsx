7cb7bc24f4b8c61e96edbe8fec7fe5a5510237d0
DilettaZecchinetti <diletta.zecchinetti@gmail.com>
2025-07-04 17:47:38 +0100

    configure React Router to handle auth routes with Clerk

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,23 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
+import { SignedIn, SignedOut, SignIn, SignUp } from "@clerk/clerk-react";
+import { Routes, Route, Link } from 'react-router-dom';
+import { Navigate } from "react-router-dom";
+import Home from "./Components/Home";
+
 import './App.css'
 
 function App() {
-
   return (
-    <header>
-      <SignedIn>
-        <h1>Welcome, you're signed in</h1>
-        <UserButton />
-      </SignedIn>
+    <Routes>
+      <Route
+        path="/home"
+        element={
+          <SignedIn>
+            <Home />
+          </SignedIn>
+        }
+      />
 
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-    </header>
+      <Route
+        path="/sign-in"
+        element={
+          <SignedOut>
+            <SignIn routing="path" path="/sign-in" />
+          </SignedOut>
+        }
+      />
+
+      <Route
+        path="/sign-up"
+        element={
+          <SignedOut>
+            <SignUp routing="path" path="/sign-up" />
+          </SignedOut>
+        }
+      />
+
+      <Route
+        path="*"
+        element={
+          <>
+            <SignedIn>
+              <Navigate to="/home" replace />
+            </SignedIn>
+            <SignedOut>
+              <Navigate to="/sign-in" replace />
+            </SignedOut>
+          </>
+        }
+      />
+    </Routes>
   );
 }
 
-export default App
+export default App;
